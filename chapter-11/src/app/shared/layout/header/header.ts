18eff2c99c10a9440f38175e201d0a766f79e066
dimeloper <dkiriakakis@gmail.com>
2025-07-25 21:37:31 +0000

    Include login link in header.

--- chapter-11/src/app/shared/layout/header/header.ts
+++ chapter-11/src/app/shared/layout/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
