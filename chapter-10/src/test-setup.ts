7b5768124aced09ba70eff7748e575ef3af37001
dimeloper <dkiriakakis@gmail.com>
2025-09-12 10:18:23 +0000

    Update unit tests and project configuration.

--- chapter-10/src/test-setup.ts
+++ chapter-10/src/test-setup.ts
@@ -1,11 +1,13 @@
 import 'zone.js';
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
 
-
-// Setup Angular testing environment
+// Setup Angular testing environment with current APIs
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
+  BrowserTestingModule,
+  platformBrowserTesting(),
 );
