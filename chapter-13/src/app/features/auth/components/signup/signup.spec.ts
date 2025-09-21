bb40d68d5d0150e1a008fd9f6541909fc370f9c4
dimeloper <dkiriakakis@gmail.com>
2025-09-21 09:39:05 +0000

    Adjust unit tests.

--- chapter-13/src/app/features/auth/components/signup/signup.spec.ts
+++ chapter-13/src/app/features/auth/components/signup/signup.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Signup } from './signup';
 
@@ -8,7 +9,7 @@ describe('Signup', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Signup]
+      imports: [Signup, RouterTestingModule]
     })
     .compileComponents();
 
--- chapter-13/src/app/features/auth/pages/signup-page/signup-page.spec.ts
+++ chapter-13/src/app/features/auth/pages/signup-page/signup-page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SignupPage } from './signup-page';
 
@@ -8,7 +9,7 @@ describe('SignupPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignupPage]
+      imports: [SignupPage, RouterTestingModule]
     })
     .compileComponents();
 
