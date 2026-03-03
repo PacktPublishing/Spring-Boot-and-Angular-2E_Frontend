8709c547f07c90ba295123c819a83eb9634cf8d6
dimeloper <dkiriakakis@gmail.com>
2026-03-03 13:41:51 +0000

    feat: prepare chapter-15 directory, clean up old dirs

--- chapter-15_v20/src/app/features/books/books.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { List } from './pages/list/list';
-import { BookForm } from './components/book-form/book-form';
-
-export const routes: Routes = [
-  { path: '', component: List },
-  { path: 'create', component: BookForm },
-  { path: 'edit/:id', component: BookForm },
-];
