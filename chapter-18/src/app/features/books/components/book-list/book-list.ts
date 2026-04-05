9be7a389617774513fa82d7da969c0258c159891
dimeloper <dkiriakakis@gmail.com>
2026-04-05 21:18:02 +0000

    include defer example

--- chapter-18/src/app/features/books/components/book-list/book-list.ts
+++ chapter-18/src/app/features/books/components/book-list/book-list.ts
@@ -5,18 +5,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { CurrencyPipe } from '@angular/common';
 import { Book } from '../../../../shared/models/book';
 import { AuthStore } from '../../../auth/store/auth.store';
+import { PageEvent, MatPaginator } from '@angular/material/paginator';
+import { bookPageEvents } from '../../store/book-store/book.events';
+import { injectDispatch } from '@ngrx/signals/events';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BookStore } from '../../store/book-store/book.store';
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.html',
   styleUrls: ['./book-list.scss'],
-  imports: [CurrencyPipe, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [
+    CurrencyPipe,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatPaginator,
+    MatProgressSpinnerModule,
+  ],
 })
 export class BookList {
   books = input<Book[]>([]);
   book = output<Book>();
   editBookEvent = output<Book>();
   deleteBookEvent = output<Book>();
+
+  protected readonly store = inject(BookStore);
+  protected readonly dispatch = injectDispatch(bookPageEvents);
 
   protected readonly authStore = inject(AuthStore);
   private readonly baseColumns = ['title', 'author', 'genre', 'price', 'published'];
@@ -34,5 +49,12 @@ export class BookList {
 
   deleteBook(book: Book) {
     this.deleteBookEvent.emit(book);
+  }
+
+  onPageChange(event: PageEvent) {
+    this.dispatch.loadBooks({
+      page: event.pageIndex,
+      size: event.pageSize,
+    });
   }
 }
